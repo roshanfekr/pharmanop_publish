2239ca4de56ba2250e903c66a7c1e398d7df0f15
roshanfekr <Unknowe@Unknowe.com>
2021-06-21 00:38:37 +0430

    MegaMenu

---
+++ Plugins/SevenSpikes.Nop.Plugins.MegaMenu/Scripts/MenuScroll.js
@@ -0,0 +1,38 @@
+﻿
+$(document).ready(function () {
+
+    window.addEventListener("scroll", function () {
+        var menu = $(".header-lower");
+        if (window.scrollY > 140) {
+            menu.addClass("fix-menu-header-top");
+            //menu.removeClass("fix-menu-header-top-relative");
+            //console.log(window.scrollY)
+        } else {
+            menu.removeClass("fix-menu-header-top");
+            //menu.addClass("fix-menu-header-top-relative");
+            //console.log('remove')
+
+        }
+    }, true);
+
+    $('#small-search-box-form').addClass("display-none");
+
+    $('.search-box-opener').on('click', function () {
+
+        if (!$(this).hasClass('open')) {
+            $('#small-search-box-form').removeClass("display-none");
+        }
+        else {
+            setTimeout(function () {
+                
+                $('#small-search-box-form').addClass('display-none');
+            }, 500); // time should match the css transition delay time
+        }
+    });
+
+
+});
+
+
+
+
